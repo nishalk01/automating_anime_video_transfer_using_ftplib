3826ce59ec240b74e2fdd7851fe130bec5e67b40
nishalk01 <45785279+nishalk01@users.noreply.github.com>
2020-06-02 01:27:02 -0400

    Update get_latest_time.py

--- get_latest_time.py
+++ get_latest_time.py
@@ -9,36 +9,23 @@ def get_time(file_name):
     date_of_file=datetime.datetime(date_striped.year,date_striped.month,date_striped.day,date_striped.hour,date_striped.minute,date_striped.second)
     return date_of_file
 
-def run_(): 
+def run():
     path=".anime/"
     all_anime_vid=[]
-    animes =os.listdir(path)
-    for anime in animes:
-        path2anime_vid=os.path.join(path,anime)
-        anime_episodes=os.listdir(path2anime_vid)
-        for i,anime_episode in enumerate(anime_episodes):
-            path2anime=os.path.join(path2anime_vid,anime_episode)
-            if(os.path.isdir(path2anime)):
-                folder_in_folder=os.listdir(path2anime)
-                for animes in folder_in_folder:
-                    folder_in_folder_vid=os.path.join(path2anime,animes)
-                    if(os.path.isfile(folder_in_folder_vid)):
-                        all_anime_vid.append(folder_in_folder_vid)
-                        
-            elif(os.path.isfile(path2anime)):
-             all_anime_vid.append(path2anime)
-
+    for root, dirs, files in os.walk(path, topdown=True):
+     for name in files:
+       file_=os.path.join(root, name)
+       if os.path.isfile(file_):
+        all_anime_vid.append(file_)
     time_of_all=[]
-    for name in all_anime_vid:
-     time_of_all.append(get_time(name))
-
-    #find latest time
+    for name_of_vid in all_anime_vid:
+        time_of_all.append(get_time(name_of_vid))
+    
     latest_time_=time_of_all[0]
     for i in range(len(time_of_all)):
         if(i<len(time_of_all)-1):
          if(latest_time_<time_of_all[i]):
            latest_time_=time_of_all[i]
-
     ind_t=time_of_all.index(latest_time_) 
     path_of_anime_ep=all_anime_vid[ind_t]
     name=os.path.basename(path_of_anime_ep)
