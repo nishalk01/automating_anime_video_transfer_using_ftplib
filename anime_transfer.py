f38f2d9e133407012826a14efa5443b430bfd785
nishalk01 <45785279+nishalk01@users.noreply.github.com>
2020-06-05 11:34:45 -0400

    Update anime_transfer.py

--- anime_transfer.py
+++ anime_transfer.py
@@ -25,22 +25,16 @@ def download(ftp,file, localdir): #downloads the file
 def convert_bytes_to_mb(file_): #converts given filename size to MB from bytes 
     return str(round(ftp.size(file_)/(1000*1000),2))
 
-name,date_to_check=run()
+all_anime_dow=check_the_list()
 for file_ in files:
-    try:
-     if(file_.split(".")[1]=="mp4"):
-      name="MDTM {}".format(file_)
-      date_time=ftp.sendcmd(name)
-      li=date_time.split(" ")
-      my_date = datetime.strptime(li[1], "%Y%m%d%H%M%S")
-      date_of_mfile_=datetime(my_date.year,my_date.month,my_date.day, my_date.hour,my_date.minute,my_date.second)
-      if(date_of_mfile_>=date_to_check):
+    if(file_ not in all_anime_dow):
+     try:
+      if(file_.split(".")[1]=="mp4"):
           file_size_in_mb=convert_bytes_to_mb(file_)
           print("downloading {} of size {} MB...".format(file_,file_size_in_mb))
           download(ftp,file_,save_downloads_)
       
-    except (ftplib.error_perm,IndexError):
-           print("error")  
-      
+     except (ftplib.error_perm,IndexError):
+           print("error")     
 
 
