cb13a156061a69487c96e88837199ba731a347d2
nishalk01 <45785279+nishalk01@users.noreply.github.com>
2020-10-15 04:43:37 -0400

    Update anime_transfer.py

--- anime_transfer.py
+++ anime_transfer.py
@@ -1,19 +1,33 @@
 import ftplib
+from tqdm import tqdm
 import os
 import time
+from dateutil import parser
 from datetime import datetime
-from get_list import check_the_list
+# from latest_mtime import  check_the_list
+path="UCDownloads/video"
+save_downloads_=".anime/"
 
-path="storage/shared/UCDownloads/video"
-save_downloads_="./anime" #make where your'e saving is inside the folder your checking the list with 
 ftp=ftplib.FTP()
 ftp.connect("192.168.43.1",1024)
+ftp.login()
+ftp.cwd(path)
+ftp.pwd()
+files=ftp.nlst(".")
+ftp.voidcmd('TYPE I')
 
 ## for verbose of process slows down download speed
 #ftp.set_debuglevel(1)  #u change value of levels for detailed verbose output
 
-ftp.cwd(path)
-files=ftp.nlst(".")
+def check_the_list(): 
+    path=".anime/"
+    all_anime_vid=[]
+    for root,dirs,files in os.walk(path,topdown=True):
+        for name in files:
+            all_anime_vid.append(os.path.basename(name))
+    return all_anime_vid
+
+
 
 def download(ftp,file, localdir): #downloads the file
     start=time.time()
@@ -26,11 +40,12 @@ def download(ftp,file, localdir): #downloads the file
 def convert_bytes_to_mb(file_): #converts given filename size to MB from bytes 
     return str(round(ftp.size(file_)/(1000*1000),2))
 
+
 all_anime_dow=check_the_list()
 for file_ in files:
     if(file_ not in all_anime_dow):
      try:
-      if(file_.split(".")[1]=="mp4"):
+      if(file_.split(".")[1]=="mp4"):#downloads only mp4
           file_size_in_mb=convert_bytes_to_mb(file_)
           print("downloading {} of size {} MB...".format(file_,file_size_in_mb))
           download(ftp,file_,save_downloads_)
